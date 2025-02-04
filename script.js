ca125cd019ace9e287ef0f190b7573aff685ac66
Amaan1911 <163126376+Amaan1911@users.noreply.github.com>
2025-02-04 21:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str.indexof(str[i])===str.lastindexof(str[i])){
+		return str[i]
+	}
+	else 
+		return null
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string")
 alert(firstNonRepeatedChar(input)); 
